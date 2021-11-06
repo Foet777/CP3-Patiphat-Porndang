16b0035d0a70551847985f2a4f49bcfd44c0c46e
Foet777 <82232788+Foet777@users.noreply.github.com>
2021-11-06 22:18:55 +0700

    Add files via upload

---
+++ Exercise4_Patiphat_P.py
@@ -0,0 +1,16 @@
+Found_eng = 'Foundation English'
+Gen_bus = 'General Business'
+Intro_com = 'Introduction To Computer Systems'
+Comprog = "Computer Progamming"
+
+F = (50)
+G = (60)
+I = (46)
+C = (45)
+
+print("---your score---")
+print((Found_eng) ,": your score is ", (F))
+print((Gen_bus),': your socre is', (G))
+print((Intro_com),': your score is', (I))
+print((Comprog),':your score is', (C))
+print("---You've done your best.---")
