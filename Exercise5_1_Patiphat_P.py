15f38ea96e4e9c88c2282b4765ca5214d875a1c1
Foet777 <82232788+Foet777@users.noreply.github.com>
2021-11-08 16:28:14 +0700

    Add files via upload

---
+++ Exercise5_1_Patiphat_P.py
@@ -0,0 +1,6 @@
+a=18
+b=3
+print(a,"+",b,"=",(a+b))
+print(a,"-",b,"=",(a-b))
+print(a,"*",b,"=",(a*b))
+print(a,"/",b,"=",int(a/b))
---
+++ Exercise5_2_Patiphat_P.py
@@ -0,0 +1,4 @@
+S = int(input("How many distance did you travel ? (unit: KM) "))
+t = int(input("How long did you take ? (unit: Hr)"))
+print(int(S/t),"Km/Hr")
+
